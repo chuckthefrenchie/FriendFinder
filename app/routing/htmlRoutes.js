351661e80ca95af2f52ff3bd092417dd8bd17fd0
Daria Korbysheva <korbyshevadaria@gmail.com>
2019-09-14 17:56:24 -0700

    updated survey.html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,12 +5,12 @@ var path = require("path");
 
 module.exports = function(app) {
 
-  app.get("/tables", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/reserve.html"));
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
