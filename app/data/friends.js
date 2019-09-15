93fde11b4bcf0b42d98e57b6f4026ed3824a7708
Daria Korbysheva <korbyshevadaria@gmail.com>
2019-09-14 17:38:12 -0700

    updated friends.js with pictures

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,52 +3,52 @@
 var friendsArray = [
     {
         "name": "Anna",
-        "photo": "https://i.pinimg.com/originals/e2/9b/90/e29b905f1573d2412e15845fd2fc97cf.jpg",
+        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Flumiere-a.akamaihd.net%2Fv1%2Fimages%2Fb5da8e4c0046a83b81dbd945719f6b354edd764b.jpeg%3Fregion%3D0%2C0%2C450%2C450&imgrefurl=https%3A%2F%2Ffrozen.disney.com%2Fanna&docid=HSFYyNrARbrbkM&tbnid=YNOFXGKmHCWaDM%3A&vet=10ahUKEwjV27r9ydHkAhXXIDQIHat0Dc0QMwhpKAAwAA..i&w=450&h=450&bih=641&biw=1280&q=anna%20picture&ved=0ahUKEwjV27r9ydHkAhXXIDQIHat0Dc0QMwhpKAAwAA&iact=mrc&uact=8",
         "scores": [1, 3, 5, 3, 2, 1, 2, 1, 4, 5]
     },
     {
         "name": "Mary",
-        "photo": "https://i.pinimg.com/originals/e2/9b/90/e29b905f1573d2412e15845fd2fc97cf.jpg",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjjzsWVytHkAhX3FTQIHS3-B0cQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.independent.ie%2Fentertainment%2Fmovies%2Fmary-poppins-returns-but-can-she-really-be-reinvented-for-our-modern-era-37602224.html&psig=AOvVaw2XWWb5EV1jAEqqJFgyS-Ro&ust=1568594002206351",
         "scores": [1, 1, 3, 3, 2, 1, 5, 5, 5, 1]
     },
     {
         "name": "Alex",
-        "photo": "https://i.pinimg.com/originals/e2/9b/90/e29b905f1573d2412e15845fd2fc97cf.jpg",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiIucSqytHkAhUULX0KHe4xCM4QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.independent.co.uk%2Farts-entertainment%2Ffilms%2Ffeatures%2Falex-lawther-interview-old-boys-end-of-the-fucking-world-netflix-oscars-a8792631.html&psig=AOvVaw0v1WQPh2p-uMAOgwJ2pYQq&ust=1568594025383775",
         "scores": [1, 1, 3, 3, 2, 1, 5, 5, 5, 1]
     },
     {
         "name": "Tom",
-        "photo": "https://i.pinimg.com/originals/e2/9b/90/e29b905f1573d2412e15845fd2fc97cf.jpg",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi-7KG1ytHkAhWKIDQIHQdnAMsQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.planetminecraft.com%2Fskin%2Finieloo-tom-tom-amp-jerry%2F&psig=AOvVaw0Wcq6m7gLJPuvajmWqVk_l&ust=1568594068833696",
         "scores": [1, 3, 5, 1, 2, 5, 5, 1, 3, 4]
     },
     {
         "name": "Mark",
-        "photo": "https://i.pinimg.com/originals/e2/9b/90/e29b905f1573d2412e15845fd2fc97cf.jpg",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi24o7AytHkAhW5IjQIHakvBCEQjRx6BAgBEAQ&url=https%3A%2F%2Fjoke-battles.fandom.com%2Fwiki%2FMark_Zuckerberg&psig=AOvVaw3f0vmgPCAYOV6DBbXZZ93T&ust=1568594089444371",
         "scores": [4, 3, 1, 3, 1, 5, 2, 2, 5, 1]
     },
     {
         "name": "Tanya",
-        "photo": "https://i.pinimg.com/originals/e2/9b/90/e29b905f1573d2412e15845fd2fc97cf.jpg",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiK3-DVytHkAhVlMn0KHaM3CWwQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.imdb.com%2Fname%2Fnm2117238%2F&psig=AOvVaw34rvO4pXO8qwMqjW5sDyDF&ust=1568594113029156",
         "scores": [3, 1, 5, 5, 3, 5, 1, 3, 4, 3]
     },
     {
         "name": "Sophie",
-        "photo": "https://i.pinimg.com/originals/e2/9b/90/e29b905f1573d2412e15845fd2fc97cf.jpg",
+        "photo": "https://s31242.pcdn.co/wp-content/uploads/2019/05/sophie_turner_loves_soul_not_gender_game_of_thrones-1024x681.jpg",
         "scores": [3, 2, 4, 1, 5, 5, 1, 1, 1, 1]
     },
     {
         "name": "John",
-        "photo": "https://i.pinimg.com/originals/e2/9b/90/e29b905f1573d2412e15845fd2fc97cf.jpg",
+        "photo": "https://www.biography.com/.image/t_share/MTE4MDAzNDEwMjg5NDYwNzUw/john-legend-201302-1-402.jpg",
         "scores": [2, 4, 1, 4, 3, 3, 1, 3, 1, 1]
     },
     {
         "name": "Deborah",
-        "photo": "https://i.pinimg.com/originals/e2/9b/90/e29b905f1573d2412e15845fd2fc97cf.jpg",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwihkPX5ytHkAhXLHDQIHbhhC88QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.bfi.org.uk%2Fnews-opinion%2Fnews-bfi%2Flists%2Fdeborah-kerr-10-essential-films&psig=AOvVaw3CO15mbUteaS99g-259iqH&ust=1568594210838032",
         "scores": [3, 3, 3, 3, 1, 3, 5, 5, 5, 1]
     },
     {
         "name": "Mitch",
-        "photo": "https://i.pinimg.com/originals/e2/9b/90/e29b905f1573d2412e15845fd2fc97cf.jpg",
+        "photo": "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiWt9iGy9HkAhXcGTQIHQT7BaQQjRx6BAgBEAQ&url=https%3A%2F%2Fthehill.com%2Fpeople%2Fmitch-mcconnell&psig=AOvVaw2TQUn2I4EVLhD20IBDUujD&ust=1568594237659523",
         "scores": [5, 5, 5, 5, 1, 1, 2, 3, 2, 4]
     },
 ];
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,19 +1,9 @@
-// ===============================================================================
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-// ===============================================================================
+
 var path = require("path");
 
-
-// ===============================================================================
 // ROUTING
-// ===============================================================================
 
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
 
   app.get("/tables", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/tables.html"));
