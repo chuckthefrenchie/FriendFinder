4dbdd92f42862bf27dbf39038bea0310eb6beec6
Daria Korbysheva <korbyshevadaria@gmail.com>
2019-09-15 19:37:30 -0700

    updated survey.html, working submit button

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,8 +7,38 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-   var newFriend = req.body;
-   console.log(newFriend);
-    
-  });
-}; // to close function
+   var newUser = req.body;
+   console.log(newUser);
+  
+
+//to compare new user to friends
+
+var differences = [];
+
+friendsData.forEach(function(friend){
+
+    var totalDifference = 0;
+
+    for (var i = 0; i < newUser.scores.length; i++){
+        var difference = friend.scores[i] - parseInt(newUser.scores[i]);
+        totalDifference += Math.abs(difference);
+        // console.log(totalDifference);
+    }
+    differences.push(totalDifference);
+});
+
+// console.log(differences);
+
+var leastDiff = Math.min.apply(null, differences);
+
+var bestMatch;
+
+for(var i = 0; i < differences.length; i++){
+    if(differences[i] === leastDiff){
+        bestMatch = friendsData[i];
+    }
+}
+res.json(bestMatch);
+friendsData.push(newUser);
+});
+}
